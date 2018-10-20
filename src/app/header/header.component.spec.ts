f6d1d64e838bf22c94c9871d069a629ad86cb7b3
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-10-20 17:29:03 -0300

    unit test header component

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { USER } from './../user';
 
-describe('HeaderComponent', () => {
+fdescribe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let compiled;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +18,43 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    compiled = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should find classes .header, .user, .avatar and .name', () => {
+    expect(compiled.querySelector('.header')).not.toBeNull();
+    expect(compiled.querySelector('.header > .user')).not.toBeNull();
+    expect(compiled.querySelector('.header > .user > .avatar')).not.toBeNull();
+    expect(compiled.querySelector('.header > .user > .avatar > img')).not.toBeNull();
+    expect(compiled.querySelector('.header > .user > .name')).not.toBeNull();
+  });
+
+  it('should bind [user] to template', () => {
+    const avatar = compiled.querySelector('.header > .user > .avatar > img');
+    expect(avatar.src.match(USER.avatar)).toBeTruthy();
+
+    const name = compiled.querySelector('.header > .user > .name');
+    expect(name.textContent).toEqual(USER.name);
+  });
+
+  it('should not found .avatar src when [user] is undefined', () => {
+    component.user = undefined;
+    fixture.detectChanges();
+
+    const avatar = compiled.querySelector('.header > .user > .avatar > img');
+    expect(avatar.src.match(USER.avatar)).not.toBeTruthy();
+  });
+
+  it('should not found .name text when [user] is undefined', () => {
+    component.user = undefined;
+    fixture.detectChanges();
+
+    const name = compiled.querySelector('.header > .user > .name');
+    expect(name.textContent).toEqual('');
   });
 });
