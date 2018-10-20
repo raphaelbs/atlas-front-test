310117a13687dd85532db47acd6aadd1dcd5675a
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-10-20 16:26:11 -0300

    unit test side-menu and side-menu-item components

--- src/app/side-menu/side-menu-item/side-menu-item.component.spec.ts
+++ src/app/side-menu/side-menu-item/side-menu-item.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SideMenuItemComponent } from './side-menu-item.component';
+import { MENU } from './../../menu';
 
 describe('SideMenuItemComponent', () => {
   let component: SideMenuItemComponent;
   let fixture: ComponentFixture<SideMenuItemComponent>;
+  let compiled;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +18,28 @@ describe('SideMenuItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SideMenuItemComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should find classes .side-menu-item, .icon and .description', () => {
+    expect(compiled.querySelector('div.side-menu-item')).not.toBeNull();
+    expect(compiled.querySelector('div.side-menu-item > div.icon')).not.toBeNull();
+    expect(compiled.querySelector('div.side-menu-item > div.description')).not.toBeNull();
+  });
+
+  it('should bind @Input [item] to template', () => {
+    component.item = MENU[0];
+    fixture.detectChanges();
+
+    const img = compiled.querySelector('div.side-menu-item > div.icon > img');
+    expect(img.src.match(MENU[0].icon)).toBeTruthy();
+    expect(img.alt).toEqual(MENU[0].description);
+
+    const description = compiled.querySelector('div.side-menu-item > div.description');
+    expect(description.textContent).toEqual(MENU[0].description);
   });
 });
