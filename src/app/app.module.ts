78715f4d458742b409ee00359575235a8d263ef1
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-10-20 00:15:14 -0300

    route and struct app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideMenuComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
