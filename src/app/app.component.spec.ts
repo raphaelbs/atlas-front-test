03ad8ff5d342155180c9a7d3ea9954029d6d9f23
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-10-21 16:37:08 -0300

    app.component unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs/Observable';
 
 import { AppComponent } from './app.component';
 
@@ -8,44 +10,58 @@ import { SideMenuComponent } from './side-menu/side-menu.component';
 import { SideMenuItemComponent } from './side-menu/side-menu-item/side-menu-item.component';
 import { FooterComponent } from './footer/footer.component';
 
+class FakeLoader implements TranslateLoader {
+  getTranslation(lang: string): Observable<any> {
+    return Observable.of(lang);
+  }
+}
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        TranslateModule.forRoot({
+          loader: {provide: TranslateLoader, useClass: FakeLoader},
+        }),
       ],
       declarations: [
         AppComponent,
         FooterComponent,
         HeaderComponent,
         SideMenuComponent,
-        SideMenuItemComponent
+        SideMenuItemComponent,
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it('should render <app-header> component', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('app-header')).not.toBeNull();
   }));
+
   it('should render <app-side-menu> component', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('app-side-menu')).not.toBeNull();
   }));
+
   it('should render <app-footer> component', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('app-footer')).not.toBeNull();
   }));
+
   it('should include <router-outlet> component', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
