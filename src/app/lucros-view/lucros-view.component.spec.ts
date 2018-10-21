1a3e013baf15b514f7027c68df13b49d6427757e
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-10-21 16:33:55 -0300

    lucros-view unit test

--- src/app/lucros-view/lucros-view.component.spec.ts
+++ src/app/lucros-view/lucros-view.component.spec.ts
@@ -1,14 +1,34 @@
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
+import { FormsModule } from '@angular/forms';
 
 import { LucrosViewComponent } from './lucros-view.component';
+import { SearchBarComponent } from './../shared/search-bar/search-bar.component';
+
+class FakeLoader implements TranslateLoader {
+  getTranslation(lang: string): Observable<any> {
+    return Observable.of(lang);
+  }
+}
 
 describe('LucrosViewComponent', () => {
   let component: LucrosViewComponent;
   let fixture: ComponentFixture<LucrosViewComponent>;
+  let compiled;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LucrosViewComponent ]
+      declarations: [
+        LucrosViewComponent,
+        SearchBarComponent,
+      ],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {provide: TranslateLoader, useClass: FakeLoader},
+        }),
+        FormsModule
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +36,36 @@ describe('LucrosViewComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LucrosViewComponent);
     component = fixture.componentInstance;
+    compiled = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain classes .lucros-view, .header, .title-sub-title, .title, .sub-title, ', () => {
+    expect(compiled.querySelector('.lucros-view')).not.toBeNull();
+    expect(compiled.querySelector('.lucros-view > .header')).not.toBeNull();
+    expect(compiled.querySelector('.lucros-view > .header > .title-sub-title')).not.toBeNull();
+    expect(compiled.querySelector('.lucros-view > .header > .title-sub-title > .title')).not.toBeNull();
+    expect(compiled.querySelector('.lucros-view > .header > .title-sub-title > .sub-title')).not.toBeNull();
+
+  });
+
+  it('should contain [app-search-bar]', () => {
+    expect(compiled.querySelector('.lucros-view > .header > app-search-bar')).not.toBeNull();
+  });
+
+  it('should contain untranslated div title', () => {
+    fixture.detectChanges();
+    const div = compiled.querySelector('.lucros-view > .header > .title-sub-title > .title');
+    expect(div.textContent).toBe('lucros-view.view-title');
+  });
+
+  it('should contain untranslated div sub-title', () => {
+    fixture.detectChanges();
+    const div = compiled.querySelector('.lucros-view > .header > .title-sub-title > .sub-title');
+    expect(div.textContent).toBe('lucros-view.view-description');
   });
 });
--- src/app/lucros-view/lucros-view.module.ts
+++ src/app/lucros-view/lucros-view.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { LucrosViewComponent } from './lucros-view.component';
 import { SharedModule } from '../shared/shared.module';
 
-export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/lucros-view/', '.json');
-}
 
 const routes: Routes = [
   {
@@ -23,16 +17,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    HttpClientModule,
     SharedModule,
-    TranslateModule.forChild({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [HttpClient]
-      },
-      useDefaultLang: true
-    })
   ],
   declarations: [
     LucrosViewComponent
