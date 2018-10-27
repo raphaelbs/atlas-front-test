6ad1a12da770010452fd3bd9fcb13800d990d690
Raphael Brandão <raphael.b.souza@hotmail.com>
2018-10-27 17:56:58 -0300

    add sonar pipeline to travis

--- protractor.conf.js
+++ protractor.conf.js
@@ -26,6 +26,8 @@ exports.config = {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    jasmine.getEnv().addReporter(
+      new SpecReporter({ spec: { displayStacktrace: true }, summary: {  } })
+    );
   }
 };
